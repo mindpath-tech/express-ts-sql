0dbd0327bde0875880c8986ad246cd7af5791636
Vikas Patidar <vikas@mindpathtech.com>
2021-09-06 13:25:35 +0530

    feature test framework integrated with coverage report

--- script/ts-node.sh
+++ script/ts-node.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 # avoid a cut-and-paste of this into every `yarn` script
-ts-node --files -P ./tsconfig.json -r tsconfig-paths/register "$@"
+ts-node --files -P ./tsconfig.build.json -r tsconfig-paths/register "$@"
