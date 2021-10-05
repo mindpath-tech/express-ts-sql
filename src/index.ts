573d8fd81b4c178a16c43821ecb6297bd283b5a1
Ritik Jain <ritik@mindpathtech.com>
2021-10-05 09:29:54 +0530

    Integrated swagger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import HttpStatus from 'http-status-codes';
 import logger from '@mindpath/logger';
+import swagger from '../src/privateLibs/swagger-generator-express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 
@@ -13,13 +16,60 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(useragent.express());
 
+const swaggerOptions = {
+  title: 'express-ts-sql',
+  version: '1.0.0',
+  host: 'localhost',
+  basePath: '/',
+  schemes: ['https', 'http'],
+  securityDefinitions: {
+    Bearer: {
+      description: 'Example value:- Bearer eyJhbGciOiJIUzI1NiJ9.eyJOYW1lIjoiUml0aWsgSmFpbiJ9.OENs7sVbpa5BpVH0LkqH5V0uuqwsfizV2u1Psa_G6R0',
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
+    },
+  },
+  parameters: {
+    timezone: {
+      description: 'Asia/Calcutta',
+      type: 'string',
+      name: 'timezone',
+      in: 'header',
+    },
+    urlDomain: {
+      description: 'localhost',
+      type: 'string',
+      name: 'urlDomain',
+      in: 'header',
+    },
+  },
+  security: [{ Bearer: [] }],
+  defaultSecurity: 'Bearer',
+};
+
 const port = serverConfig.port;
+
+fs.readdirSync(path.resolve(__dirname, 'routes', 'v1')).forEach((file) => {
+  if (file.includes('.js') && !file.includes('.js.')) {
+    console.log(file);
+    const { router, basePath } = require(`./routes/v1/${file}`);
+    app.use(basePath, router);
+  }
+});
+
 app.get('/health', function (req, res) {
   res.status(200).send('ok');
 });
 
 const server = app.listen(port, () => {
   console.info(`Started on port : ${port}`);
+});
+
+swagger.serveSwagger(app, '/swagger', swaggerOptions, {
+  routePath: './dist/src/routes/v1',
+  requestModelPath: './dist/src/requestModels',
+  responseModelPath: './dist/src/responseModels',
 });
 
 app.get('*', function (req: Request, res: Response) {
